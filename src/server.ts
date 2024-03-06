9bcb2c66b447be2fc4379d9e7643d9a49a6d7414
Rafael Aguiar <terule@gmail.com>
2024-03-06 06:40:14 -0300

    feat: Add dotenv configuration for environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
 import app from "./app";
 
 const port = process.env.PORT || 3001;
-
-dotenv.config();
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
