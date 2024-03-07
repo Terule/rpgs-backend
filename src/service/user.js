a033196a233438b6b8c48ff5e0f87f4c5b44a4d1
Rafael Aguiar <terule@gmail.com>
2024-03-07 19:40:25 -0300

    feat: Add user service and model classes

--- src/service/user.js
+++
@@ -1,6 +0,0 @@
-const model = require('../model/userModel');
-
-export const createUserService = async ({name, email, password, isActive = true, isAdmin = false, imageUrl = null}) => {
-  const user = await model.createUser({name, email, password, isActive, isAdmin, imageUrl});
-  return user;
-}
