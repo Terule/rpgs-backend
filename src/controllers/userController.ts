fb3a6af5bcc8d5bf95fbde6664a40cc755e631b6
Rafael Aguiar <terule@gmail.com>
2024-03-08 12:19:44 -0300

    refactor: Update method names in userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,58 +2,67 @@ import { Response, Request } from 'express';
 import { userData } from "@cTypes/userTypes";
 import { UserService } from "@services/userService";
 
-export class userController {
+export class UserController {
   private _userService: UserService;
 
   constructor(userService: UserService) {
     this._userService = userService;
   }
 
-  public createUser = async (req: Request, res: Response) => {
+  public create = async (req: Request, res: Response) => {
     try {
       const userData: userData = req.body;
-      const user = await this._userService.createUser(userData);
+      const user = await this._userService.create(userData);
       res.status(201).json(user);
     } catch (error) {
       res.status(500).json({ error: (error as Error).message });
     }
   };
 
-  public getUserByEmail = async (req: Request, res: Response) => {
+  public getAll = async (req: Request, res: Response) => {
+    try {
+      const users = await this._userService.getAll();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: (error as Error).message });
+    }
+  };
+
+  public getByEmail = async (req: Request, res: Response) => {
     try {
       const email: string = req.params.email;
-      const user = await this._userService.getUserByEmail(email);
+      const user = await this._userService.getByEmail(email);
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ error: (error as Error).message });
     }
   };
 
-  public getUserById = async (req: Request, res: Response) => {
+  public getById = async (req: Request, res: Response) => {
     try {
       const id: string = req.params.id;
-      const user = await this._userService.getUserById(id);
+      const user = await this._userService.getById(id);
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ error: (error as Error).message });
     }
   };
 
-  public updateUser = async (req: Request, res: Response) => {
+  public update = async (req: Request, res: Response) => {
     try {
       const id: string = req.params.id;
       const userData: userData = req.body;
-      const user = await this._userService.updateUser(id, userData);
+      const user = await this._userService.update(id, userData);
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ error: (error as Error).message });
     }
   };
 
-  public deleteUser = async (req: Request, res: Response) => {
+  public delete = async (req: Request, res: Response) => {
     try {
       const id: string = req.params.id;
-      const user = await this._userService.deleteUser(id);
+      const user = await this._userService.delete(id);
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ error: (error as Error).message });
