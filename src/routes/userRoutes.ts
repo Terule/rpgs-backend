d67992439043362995e14b0fff42c1b8685a974c
Rafael Aguiar <terule@gmail.com>
2024-03-09 07:17:09 -0300

    feat: Add create route to userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-
+import prisma from "@utils/prismaClient";
 import { UserController } from "@controllers/userController";
 import { UserService } from "@services/userService";
 import { UserModel } from "@models/userModel";
-import prisma from "@utils/prismaClient";
 
 
 const route = Router();
@@ -13,10 +12,10 @@ const userService = new UserService(userModel);
 const userController = new UserController(userService);
 
 route.get("/", userController.getAll);
-route.get("/:email", userController.getByEmail);
+route.post("/", userController.create);
 route.get("/:id", userController.getById);
-route.post("/create", userController.create);
 route.put("/:id", userController.update);
 route.delete("/:id", userController.delete);
+route.get("/:email", userController.getByEmail);
 
 export default route;
