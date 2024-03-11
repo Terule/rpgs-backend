928d6932f2bf4b7690f2bea001809f2cfe0799c3
Rafael Aguiar <terule@gmail.com>
2024-03-11 15:48:13 -0300

    feat: Add file upload functionality to RegisterController

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,5 +1,8 @@
+import { userData } from '@cTypes/userTypes';
 import { UserService } from '@services/userService';
-import { Request, Response } from 'express';
+import { uploadSingle } from '@utils/multerConfig';
+import { NextFunction, Request, Response } from 'express';
+import fs from 'fs';
 
 export class RegisterController {
   private _service: UserService;
@@ -9,6 +12,25 @@ export class RegisterController {
   }
   
   public register = async (req: Request, res: Response) => {
-    console.log(req.file);
+      try {
+        const {email, name, password}: userData = req.body;
+        const imagePath = req.file ? req.file.path : null;
+        await this._service.create({email, name, password, imageUrl: imagePath});
+        res.status(201).json({ message: "User created" });
+      } catch (error) {
+        if (req.file) {
+          fs.unlinkSync(req.file.path);
+        }
+        res.status(500).json({ error: (error as Error).message });
+      }
+  };
+
+  public upload = (req: Request, res: Response, next: NextFunction) => {
+    uploadSingle(req, res, (err) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      }
+      next();
+    });
   };
 }
