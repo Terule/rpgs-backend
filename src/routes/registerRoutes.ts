f9250fc1315e04f6bed629e4b643d831c64b579f
Rafael Aguiar <terule@gmail.com>
2024-03-12 09:10:07 -0300

    refactor: Update middleware import in registerRoutes file

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -3,14 +3,16 @@ import prisma from "@utils/prismaClient";
 import { RegisterController } from "@controllers/registerController";
 import { UserService } from "@services/userService";
 import { UserModel } from "@models/userModel";
-import { uploadSingle } from "@utils/multerConfig";
+import { Middlewares } from "@middlewares/middlewares";
+
 
 const route = Router();
 
 const userModel = new UserModel(prisma);
 const userService = new UserService(userModel);
 const registerController = new RegisterController(userService);
+const middlewares = new Middlewares
 
-route.post("/", registerController.upload , registerController.register);
+route.post("/", middlewares.upload , registerController.register);
 
 export default route;
