3709e497b6003988ebabd97095c28b3d6a09ce20
Rafael Aguiar <terule@gmail.com>
2024-03-25 06:16:03 -0300

    feat: Update userController to use default export for controller functions

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,61 +1,66 @@
-import { Response, Request } from 'express';
 import { userData } from "@cTypes/userTypes";
-import { UserService } from "@services/userService";
+import userService from "@services/userService";
+import { NextFunction, Request, Response } from "express";
+import fs from "fs";
 
-export class UserController {
-  private _userService: UserService;
-
-  constructor(userService: UserService) {
-    this._userService = userService;
+const create = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, name, password }: userData = req.body;
+  const imageUrl = req.file ? req.file.path : null;
+  try {
+    await userService.create({ email, name, password, imageUrl });
+    res.status(201).json({ message: "User created" });
+  } catch (e) {
+    if (imageUrl) {
+      fs.unlinkSync(imageUrl);
+    }
+    next(e);
   }
+};
 
-  public create = async (req: Request, res: Response) => {
-    try {
-      const userData: userData = req.body;
-      const user = await this._userService.create(userData);
-      res.status(201).json(user);
-    } catch (error) {
-      res.status(500).json({ error: (error as Error).message });
-    }
-  };
+const getAll = async (_req: Request, res: Response) => {
+  try {
+    const users = await userService.getAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
 
-  public getAll = async (_req: Request, res: Response) => {
-    try {
-      const users = await this._userService.getAll();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ error: (error as Error).message });
-    }
-  };
+const getById = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const user = await userService.getById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
 
-  public getById = async (req: Request, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const user = await this._userService.getById(id);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).json({ error: (error as Error).message });
-    }
-  };
+const update = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const userData: userData = req.body;
+    const user = await userService.update(id, userData);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
 
-  public update = async (req: Request, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const userData: userData = req.body;
-      const user = await this._userService.update(id, userData);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).json({ error: (error as Error).message });
-    }
-  };
+const remove = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const user = await userService.remove(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
 
-  public delete = async (req: Request, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const user = await this._userService.delete(id);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).json({ error: (error as Error).message });
-    }
-  };
-}
+export default {
+  create,
+  getAll,
+  getById,
+  update,
+  remove,
+};
