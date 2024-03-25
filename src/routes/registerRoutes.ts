150a89a17ca2ae3a95cc0ca8785d8e7c55f7d0be
Rafael Aguiar <terule@gmail.com>
2024-03-25 06:13:59 -0300

    refactor: Update import statements in registerRoutes file

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -1,18 +1,11 @@
-import { Router, request } from "express";
-import prisma from "@utils/prismaClient";
-import { RegisterController } from "@controllers/registerController";
-import { UserService } from "@services/userService";
-import { UserModel } from "@models/userModel";
+import registerController from "@controllers/registerController";
 import { Middlewares } from "@middlewares/middlewares";
-
+import { Router } from "express";
 
 const route = Router();
 
-const userModel = new UserModel(prisma);
-const userService = new UserService(userModel);
-const registerController = new RegisterController(userService);
-const middlewares = new Middlewares
+const middlewares = new Middlewares();
 
-route.post("/", middlewares.upload , registerController.register);
+route.post("/", middlewares.upload, registerController.register);
 
 export default route;
