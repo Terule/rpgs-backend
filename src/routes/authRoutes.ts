ebc42c1315cf162899e4168bf0ba29dbd0f7f1dd
Rafael Aguiar <terule@gmail.com>
2024-03-25 06:13:06 -0300

    refactor: Remove unnecessary imports and declarations in authRoutes file

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,20 +1,7 @@
+import authController from "@controllers/authController";
 import { Router } from "express";
-import prisma from "@utils/prismaClient";
-import { AuthController } from "@controllers/authController"; 
-import { UserService } from "@services/userService";
-import { UserModel } from "@models/userModel";
-import { JwtFunctions } from "@jwt/jwtFunctions";
 
 const route = Router();
-
-const jwt = new JwtFunctions();
-const config = {
-  expiresIn: '1d'
-};
-
-const userModel = new UserModel(prisma);
-const userService = new UserService(userModel);
-const authController = new AuthController(userService, jwt, config);
 
 route.post("/login", authController.login);
 route.post("/verify", authController.verify);
