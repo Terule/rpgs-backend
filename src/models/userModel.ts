6fbb780d19cf524d1a0f54d3666b65b7876443c7
Rafael Aguiar <terule@gmail.com>
2024-03-25 06:10:33 -0300

    refactor: Simplify UserModel by extracting methods into separate functions

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,15 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import AppError from "@errors/AppError";
+import { Prisma } from "@prisma/client";
+import prisma from "@utils/prismaClient";
 import { userData } from "types/userTypes";
 
-export class UserModel {
-  private _prisma: PrismaClient;
-
-  constructor(prisma: PrismaClient) {
-    this._prisma = prisma;
-  }
-
-  public create = async (userData: userData) => {
-    const user = await this._prisma.user.create({
+const create = async (userData: userData) => {
+  try {
+    const user = await prisma.user.create({
       data: userData,
       select: {
         id: true,
@@ -21,48 +17,76 @@ export class UserModel {
       },
     });
     return user;
-  };
-
-  public getByEmail = async (email: string) => {
-    const user = await this._prisma.user.findUnique({
-      where: { email },
-    });
-    return user;
-  };
-
-  public getById = async (id: string) => {
-    const user = await this._prisma.user.findUnique({
-      where: { id },
-    });
-    return user;
-  };
-
-  public getAll = async () => {
-    const users = await this._prisma.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        isAdmin: true,
-        isActive: true,
-        imageUrl: true,
-      },
-    });
-    return users;
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (error.code) {
+        case "P2002":
+          throw new AppError(400, "Email already in use");
+        case "P1000":
+          throw new AppError(503, "Database host user name or password is incorrect");
+        case "P1001":
+          throw new AppError(503, "Cannot connect to database host");
+        case "P1002":	
+          throw new AppError(408, "Database connection timed out");
+        case "P1003":
+          throw new AppError(503, "Database does not exist");
+        default:
+          throw new AppError(500, "Internal server error");
+      }
+    }
+    throw new AppError(500, (error as Error).message);
   }
+};
 
-  public update = async (id: string, userData: userData) => {
-    const user = await this._prisma.user.update({
-      where: { id },
-      data: userData,
-    });
-    return user;
-  };
+const getByEmail = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  return user;
+};
 
-  public delete = async (id: string) => {
-    const user = await this._prisma.user.delete({
-      where: { id },
-    });
-    return user;
-  };
-}
+const getById = async (id: string) => {
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+  return user;
+};
+
+const getAll = async () => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      isAdmin: true,
+      isActive: true,
+      imageUrl: true,
+    },
+  });
+  return users;
+};
+
+const update = async (id: string, userData: userData) => {
+  const user = await prisma.user.update({
+    where: { id },
+    data: userData,
+  });
+
+  return user;
+};
+
+const remove = async (id: string) => {
+  const user = await prisma.user.delete({
+    where: { id },
+  });
+  return user;
+};
+
+export default {
+  create,
+  getByEmail,
+  getById,
+  getAll,
+  update,
+  remove,
+};
