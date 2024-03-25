bf6138321a1ab71d6a68330feb88a4d0205acec1
Rafael Aguiar <terule@gmail.com>
2024-03-25 06:15:44 -0300

    feat: Refactor registerController to use ES6 syntax and export default object

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,36 +1,22 @@
-import { userData } from '@cTypes/userTypes';
-import { UserService } from '@services/userService';
-import { uploadSingle } from '@utils/multerConfig';
-import { NextFunction, Request, Response } from 'express';
-import fs from 'fs';
+import { userData } from "@cTypes/userTypes";
+import userService from "@services/userService";
+import { NextFunction, Request, Response } from "express";
+import fs from "fs";
 
-export class RegisterController {
-  private _service: UserService;
-
-  constructor(service: UserService) {
-    this._service = service;
+const register = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, name, password }: userData = req.body;
+  const imageUrl = req.file ? req.file.path : null;
+  try {
+    await userService.create({ email, name, password, imageUrl });
+    res.status(201).json({ message: "User created"});
+  } catch (e) {
+    if (imageUrl) {
+      fs.unlinkSync(imageUrl);
+    }
+    next(e);
   }
-  
-  public register = async (req: Request, res: Response) => {
-      try {
-        const {email, name, password}: userData = req.body;
-        const imagePath = req.file ? req.file.path : null;
-        await this._service.create({email, name, password, imageUrl: imagePath});
-        res.status(201).json({ message: "User created" });
-      } catch (error) {
-        if (req.file) {
-          fs.unlinkSync(req.file.path);
-        }
-        res.status(500).json({ error: (error as Error).message });
-      }
-  };
+};
 
-  public upload = (req: Request, res: Response, next: NextFunction) => {
-    uploadSingle(req, res, (err) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      }
-      next();
-    });
-  };
-}
+export default {
+  register,
+};
